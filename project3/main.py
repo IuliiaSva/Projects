c3e3dcc1b16438344daeb3f8df00d9f73b61474f
Iuliia Suslova <bacspi@icloud.com>
2025-10-05 16:13:03 +0300

    change

---
+++ project3/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/Дневник программиста', methods = ['POST'])
+def submit():
+    name=request.form['name']
+    return render_template('index.html', name=name)
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
