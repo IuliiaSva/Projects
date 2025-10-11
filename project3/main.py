82d408a200aebb0d437d26e0349a6fe690c08c65
Iuliia Suslova <bacspi@icloud.com>
2025-10-11 17:42:17 +0300

    Сохранение

--- project3/main.py
+++ project3/main.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+records={}
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/Дневник программиста', methods = ['POST'])
-def submit():
-    name=request.form['name']
-    return render_template('index.html', name=name)
-    return render_template('index.html')
+@app.route('/Дневник_программиста', methods=['GET', 'POST'])
+def notes():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        if title and text:
+            records[title] = text
+    return render_template('notes.html', records=records)
+records ["Третья тема"]="Третья тема выполнена"
 if __name__ == '__main__':
     app.run(debug=True)
